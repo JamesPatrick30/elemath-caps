926a7f875735c708013d19f4632c0cf750859ddd
Jamespatrick30 <mrsniper1388@gmail.com>
2026-07-27 21:46:49 +0800

    feat(shared): implement shared hash

---
+++ apps/api/src/shared/shared.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+@Injectable()
+export class SharedService {
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds = 10;
+    return bcrypt.hash(password, saltRounds);
+  }
+
+  async comparePasswords(password: string, hashedPassword: string): Promise<boolean> {
+    return bcrypt.compare(password, hashedPassword);
+  }
+}
