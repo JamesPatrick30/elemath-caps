27a6510dc3b2f6a1163c5391de6e5baceb0880f3
Jamespatrick30 <mrsniper1388@gmail.com>
2026-08-03 19:02:11 +0800

    feat(redis): implement event listener

--- apps/api/src/redis/redis.provider.ts
+++ apps/api/src/redis/redis.provider.ts
@@ -1,18 +1,58 @@
 import Redis from 'ioredis';
-
+import { Logger } from '@nestjs/common';
 export const REDIS = 'REDIS';
 export const REDIS_PUBLISHER = 'REDIS_PUBLISHER';
 export const REDIS_SUBSCRIBER = 'REDIS_SUBSCRIBER';
 
+const logger = new Logger('RedisProvider');
 const createRedis = () => {
-  const redis = new Redis(process.env.REDIS_URL || "");
+  const redis = new Redis(process.env.REDIS_URL!, {
+    lazyConnect: false,
+
+    retryStrategy(times) {
+      const delay = Math.min(times * 1000, 10000);
+
+      logger.warn(`Reconnect attempt #${times}`);
+
+      return delay;
+    },
+
+    maxRetriesPerRequest: null,
+  });
+
+  let lastError = 0;
+  let lastReconnect = 0;
+  let lastClose = 0;
 
   redis.on('ready', () => {
-    console.log('✅ Redis connected');
+    logger.log('✅ Redis connected');
+    lastError = 0;
+    lastReconnect = 0;
+    lastClose = 0;
   });
 
   redis.on('error', (err) => {
-    console.error('❌ Redis Error:', err.message);
+    const now = Date.now();
+    if (now - lastError > 6000) {
+      logger.error('❌ Redis Error:', err.message);
+      lastError = now;
+    }
+  });
+
+  redis.on('close', () => {
+    const now = Date.now();
+    if (now - lastClose > 6000) {
+      logger.warn('Redis connection closed');
+      lastClose = now;
+    }
+  });
+
+  redis.on('reconnecting', () => {
+    const now = Date.now();
+    if (now - lastReconnect > 6000) {
+      logger.warn('Redis reconnecting...');
+      lastReconnect = now;
+    }
   });
 
   return redis;
